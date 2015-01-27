a94c145d10f1c9f1778bf228a2f9199cc40f5648
pdostal <git@pdostal.cz>
2015-01-27 22:53:36 +0100

    Make basic search

--- app.rb
+++ app.rb
@@ -15,11 +15,30 @@ get '/' do
   clnt = HTTPClient.new
 
   uri = 'http://kdd.cz/'
-  body = { 'login[username]' => params[:u], 'login[password]' => params[:p] }
-  res = clnt.post(uri, body)
-  
-  uri = 'http://kdd.cz/index.php?page=search&lang=en'
-  res = clnt.get(uri, :follow_redirect => true)
+  query = {
+    'login[username]' => params[:u],
+    'login[password]' => params[:p]
+  }
+  res = clnt.post(uri, query)
+
+  uri = 'http://kdd.cz/index.php'
+  query = {
+    'page' => 'user',
+    'lang' => 'en'
+  }
+  res = clnt.get(uri, query)
+
+  uri = 'http://kdd.cz/index.php'
+  query = {
+    'page' => 'search',
+    'akce' => 'Search',
+    'base-search[column]' => 'vse',
+    'base-search[language]' => 'all',
+    'base-search[count-per-page]' => '99',
+    'base-search[string]' => params[:q]
+  }
+  res = clnt.get(uri, query)
+  @body = res.inspect
 
   erb :index
 end
